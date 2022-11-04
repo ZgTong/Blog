4d78929e3b1277d1976860eea2a5d37153847f21
Zuguang Tong <zuguang.tong@hertz.com>
2022-11-04 16:10:50 +1100

    project structure 1.0

--- BlogServer/src/app.ts
+++ BlogServer/src/app.ts
@@ -1,4 +1,34 @@
 import express from "express";
+import { NODE_ENV, PORT } from "@config";
+import { Routes } from "@interfaces/routes.interface";
 class App {
-    constructor(parameters) {}
+    public app: express.Application;
+    public env: string;
+    public port: string | number;
+
+    constructor(routes: Routes[]) {
+        this.app = express();
+        this.env = NODE_ENV || "development";
+        this.port = PORT || 8080;
+        this.initializeRoutes(routes);
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`=================================`)
+            console.log(`======= ENV: ${this.env} =======`)
+            console.log(`🚀 App listening on the port ${this.port}`)
+            console.log(`=================================`)
+        })
+    }
+
+    public getServer() {
+        return this.app;
+    }
+
+    public initializeRoutes(routes: Routes[]){
+        routes.forEach(route => this.app.use("/", route.router));
+    }
 }
+
+export default App;
