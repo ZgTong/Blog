1dc546976f63def4b0b210885577cf90f1cb20a6
Zuguang Tong <zuguang.tong@hertz.com>
2022-12-13 17:01:29 +1100

    daily dev

--- BlogServer/src/app.ts
+++ BlogServer/src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { NODE_ENV, PORT } from "@config";
 import { Routes } from "@interfaces/routes.interface";
-import responseHeader from "@middlewares/responseHeader.middleware";
-import errorMiddleware from "@middlewares/error.middleware";
+import initializeMiddlewares from "@middlewares/index";
 import { logger, stream } from "@utils/logger";
+import dbConnection from "@utils/dbConnection";
 
 class App {
     public app: express.Application;
@@ -15,18 +15,18 @@ class App {
         this.env = NODE_ENV || "development";
         this.port = PORT || 8080;
 
-        this.initializeMiddlewares();
+        initializeMiddlewares(this.app);
         this.initializeRoutes(routes);
-        this.initializeErrorHandling();
     }
 
-    public listen() {
+    public async listen() {
         this.app.listen(this.port, () => {
             logger.info(`=================================`)
             logger.info(`======= ENV: ${this.env} =======`)
             logger.info(`🚀 App listening on the port ${this.port}`)
             logger.info(`=================================`)
         })
+        await dbConnection();
     }
 
     public getServer() {
@@ -35,16 +35,6 @@ class App {
 
     private initializeRoutes(routes: Routes[]){
         routes.forEach(route => this.app.use("/", route.router));
-    }
-
-    private initializeErrorHandling() {
-        this.app.use(errorMiddleware);
-    }
-
-    private initializeMiddlewares() {
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(responseHeader);
     }
 }
 
--- BlogServer/src/config/index.ts
+++ BlogServer/src/config/index.ts
@@ -2,4 +2,7 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, ORIGIN, LOG_DIR, LOG_FORMAT } = process.env;
+export const {
+    NODE_ENV, PORT, SECRET_KEY, ORIGIN, LOG_DIR, LOG_FORMAT,
+    DB_URL, DB_AUTH_SOURCE, DB_USER, DB_PASSWORD
+} = process.env;
